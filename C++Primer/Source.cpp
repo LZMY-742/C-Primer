cb7292613b2cda6327457e202420d0573b8ebc92
禹锜 颜 <yyq20040730@163.com>
2022-01-28 10:41:50 +0800

    5.17

--- C++Primer/Source.cpp
+++ C++Primer/Source.cpp
@@ -21,7 +21,19 @@ int size(int(&a)[6])
 }
 int main()
 {
+	vector<int> My_vec1{ 0,1,2,3,4 };
+	vector<int> My_vec2{ 0,1,2,3,4,5 };
+	int small_size = My_vec1.size() > My_vec2.size() ? My_vec2.size() : My_vec1.size();//挑出较小者
+	for (int i = 0; i < small_size; i++)
+	{
+		if (My_vec1[i] != My_vec2[i])
+		{
+			cout << "False" << endl;
+			return 0;//如有不匹配的元素即输出False结束
+		}
+	}
+	cout << "True" << endl;
+	return 0;//如果用void main(),可以用一个flag来表示是否每个元素都相等
 	
-	return 0;
 }
 
