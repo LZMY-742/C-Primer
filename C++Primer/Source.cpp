6095eb7f0acd22b3e733e2e3792b3b7a9e579a64
禹锜 颜 <yyq20040730@163.com>
2022-02-01 12:29:39 +0800

    5.25

--- C++Primer/Source.cpp
+++ C++Primer/Source.cpp
@@ -1,9 +1,7 @@
 #include <string>
 #include "Sales_data.h"
 #include "vector"
-//#include "iterator"
-
-//#include "cctype"
+#include "stdexcept"
 extern const int b = 2;
 void increment(int i);
 using std::cout;
@@ -14,6 +12,7 @@ using std::vector;
 using std::begin;
 using std::end;
 using std::strcat;
+using std::runtime_error;
 int size(int(&a)[6])
 {
 	int b[sizeof(a)/sizeof(*a)];
@@ -21,14 +20,30 @@ int size(int(&a)[6])
 }
 int main()
 {
-	int sz;
-	while (cin >> sz)
+	int a,b;
+	cout << "请输入被除数和除数：";
+	while (cin >> a >> b)
 	{
-		if (sz <= 0)
-			continue;
-		break;
+		try
+		{
+			if (b == 0)
+				throw runtime_error("除数不得为0");//抛出异常后立即开始寻找对应的处理方式，终止try块内接下来的语句
+			cout << a / b << endl;
+			cout << "请输入被除数和除数：";
+		}
+		catch (runtime_error err)//err是runtime_error类的一个实例，实例取任何名字都行
+		{
+			cout << err.what() << endl;
+			cout << "Try again? Enter y/n: ";
+			char c;
+			cin >> c;
+			if (!cin || c == 'n')
+				break;
+			cout << "请输入被除数和除数：";
+		}
 	}
-	return 0;
 	
+
+	return 0;
 }
 
