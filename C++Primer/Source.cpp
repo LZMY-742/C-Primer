d1b59b93235844e8965e7ee74ded5231102d5176
禹锜 颜 <yyq20040730@163.com>
2022-02-04 12:34:47 +0800

    6.6 & 6.7

--- C++Primer/Source.cpp
+++ C++Primer/Source.cpp
@@ -35,8 +35,21 @@ double absolute(double val)//6.5
 		return -val;*/
 	return abs(val);
 }
+int count()//6.7
+{
+	static int jj;
+	return jj++;
+}
+void distinguish_variable(int para)//6.6
+{
+	int instance = 0;
+	static int static_variable =9;
+	//para和instance每次调用函数时都重新创建，static_variable在函数第一次被调用时被创建，直到程序结束才被销毁
+}
 int main()
 {
+	cout << count() << endl;
+	cout << count() << endl;
 	return 0;
 }
 
