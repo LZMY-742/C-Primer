c38ee5f16d8e1c5c25ac325a753d3a36123d4946
禹锜 颜 <yyq20040730@163.com>
2022-02-11 18:31:13 +0800

    6.44

--- C++Primer/Chapter6.h
+++ C++Primer/Chapter6.h
@@ -1,5 +1,11 @@
+#include "string"
+using std::string;
 #pragma once
 int jieCheng();//6.4
 double absolute(double val);//6.5
 void distinguish_variable(int para);//6.6
 int count();//6.7
+inline bool isShorter(const string& s1, const string& s2)//6.44
+{
+	return s1.size() < s2.size();
+}
