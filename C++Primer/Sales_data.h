b1b19b8596635a6b905cbfd4092e77e8975fbdac
禹锜 颜 <yyq20040730@163.com>
2022-02-18 10:25:08 +0800

    7.2

--- C++Primer/Sales_data.h
+++ C++Primer/Sales_data.h
@@ -2,10 +2,22 @@
 #define SALES_DATA
 #include <string>
 #include <iostream>
+using std::string;
 struct Sales_data
 {
 	double revenue;
 	unsigned units_sold;
-	std::string bookNo;
+	string bookNo;
+	Sales_data& combine(const Sales_data& rhs)
+	{
+		revenue += rhs.revenue;
+		units_sold += rhs.revenue;
+		return *this;
+	}
+	string isbn();
 };
+string Sales_data::isbn()
+{
+	return bookNo;
+}
 #endif
