7d9a13178f7b185587982f96b841cb8659209af7
禹锜 颜 <yyq20040730@163.com>
2022-02-20 12:51:59 +0800

    7.32

--- C++Primer/Screen.h
+++ C++Primer/Screen.h
@@ -1,11 +1,26 @@
+#pragma
 #include "string"
+#include <iostream>
 using std::string;
-class Screen {
+class Screen;
+class Window_mgr {
 public:
+	using ScreenIndex = std::vector<Screen>::size_type;
+	void clear(Screen&);
+	Window_mgr() = default;
+private:
+	//std::vector<Screen> screens{ Screen(24,80,' ') };
+};
+class Screen {
+	friend void Window_mgr::clear(Screen& s);
+public:
+
 	typedef string::size_type pos;
 	Screen() = default;
-	Screen(char c, pos h, pos w) :height(h), width(w), contents(h* w, c) {}
+	Screen(pos h, pos w,char c) :height(h), width(w), contents(h* w, c) {}
 	Screen(pos h, pos w): height(h),width(w),contents(h*w,' '){}//7.24
+
+	Screen& set(char c);
 	Screen& move(pos r, pos c)
 	{
 		pos row = r * width;
@@ -17,8 +32,33 @@ public:
 		pos row = r * width;
 		return contents[row + c];
 	}
+	Screen& display(std::ostream& os)
+	{
+		do_display(os);
+		return *this;
+	}
+	const Screen& display(std::ostream& os) const//根据调用对象是否是const判断调用哪个函数
+	{
+		do_display(os);
+		return *this;
+	}
 private:
 	pos cursor = 0;
 	pos height = 0, width = 0;
 	string contents;
+	void do_display(std::ostream& os) const
+	{
+		os << contents<<endl;
+	}
 };
+
+inline Screen& Screen::set(char c)
+{
+	contents[cursor] = c;
+	return *this;
+}
+
+void Window_mgr::clear(Screen& s)
+{
+	s.contents = std::string(s.height*s.width, ' ');
+}
